8b392a7c86edce8a17e421aa2dc815a479743f98
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2021-11-27 20:59:28 +0530

    Binary search code update

--- Binary Search/src/com/company/Main.java
+++ Binary Search/src/com/company/Main.java
@@ -7,4 +7,24 @@ public class Main {
 
 
     }
+
+    public static int binarySearch(int[] arr, int arrSize, int target){
+        int start = 0;
+        int end = arr.length -1;
+
+        while (start <= end) {
+            int mid = start/2 + end/2;
+
+            if (target < arr[mid]){
+                end = mid - 1;
+            } else if (target > arr[mid]){
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+
+        }
+        return -1;
+
+    }
 }
