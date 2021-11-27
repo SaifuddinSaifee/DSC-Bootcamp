8bb797fc39a023b25eb2ec7f17f09ab4378bf011
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2021-11-27 21:15:11 +0530

    Binary search code with example. (for ascending array)

--- Binary Search/src/com/company/Main.java
+++ Binary Search/src/com/company/Main.java
@@ -6,18 +6,18 @@ public class Main {
         // write your code here
         int[] arr = {-18, -12, -14, 0, 2, 3, 4, 15, 16, 22, 45, 89};
         int target = 22;
-        int ans = binarySearch(arr, target);
+        int ans = ascBinarySearch(arr, target);
         System.out.println(ans); // must print 9
 
 
     }
-
-    public static int binarySearch(int[] arr, int target) {
+    // this is for a code sort in ascending order
+    public static int ascBinarySearch(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
 
         while (start <= end) {
-            int mid = start / 2 + end / 2;
+            int mid = (start / 2) + (end / 2);
 
             if (target < arr[mid]) {
                 end = mid - 1;
