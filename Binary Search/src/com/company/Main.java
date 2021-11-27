d5d360934e949e23ab90cae1ce55da0a8fc565ae
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2021-11-27 21:02:40 +0530

    Binary search code with example.

--- Binary Search/src/com/company/Main.java
+++ Binary Search/src/com/company/Main.java
@@ -3,21 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        int[] arr = {-18, -12, -14, 0, 2, 3, 4, 15, 16, 22, 45, 89};
+        int target = 22;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans); // must print 9
 
 
     }
 
-    public static int binarySearch(int[] arr, int arrSize, int target){
+    public static int binarySearch(int[] arr, int target) {
         int start = 0;
-        int end = arr.length -1;
+        int end = arr.length - 1;
 
         while (start <= end) {
-            int mid = start/2 + end/2;
+            int mid = start / 2 + end / 2;
 
-            if (target < arr[mid]){
+            if (target < arr[mid]) {
                 end = mid - 1;
-            } else if (target > arr[mid]){
+            } else if (target > arr[mid]) {
                 start = mid + 1;
             } else {
                 return mid;
